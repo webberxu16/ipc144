3df095c3bb598b86786597a4450070f598bb8364
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2014-10-15 16:19:27 -0400

    w3.c

--- ipc144r/10_Oct15/w2.c
+++ ipc144r/10_Oct15/w2.c
@@ -1,11 +1,11 @@
-//2- determine the exact output of the following program [1 mark]
+//2 - determine the exact output of the following program[1 mark] 
 #include <stdio.h>
 int main(void){
    int
       i;
-   for(i=0;i<1000;i++){
-      i+=2;
+   for (i = 0; i<10000; i++){
+      i += 2;
    }
-   printf("%d",i);
+   printf("%d", i);
    return 0;
 }
