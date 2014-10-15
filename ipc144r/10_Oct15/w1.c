337a58dc729c5d141c29987765d5bfa96d0f4ddb
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2014-10-15 16:06:13 -0400

    w2.c

--- ipc144r/10_Oct15/w1.c
+++ ipc144r/10_Oct15/w1.c
@@ -1,4 +1,4 @@
-1- determine the exact output of the following program [2 marks]
+//1- determine the exact output of the following program [2 marks]
 #include <stdio.h>
 int main(void){
    printf(" his\b\bat is a test \rT\nFor \"U\" and \that is mine");
