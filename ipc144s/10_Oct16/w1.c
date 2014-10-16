7eab4ac208b92f0a11e9811be55c81c08e43a5d7
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2014-10-16 08:15:55 -0400

    w1.c

---
+++ ipc144s/10_Oct16/w1.c
@@ -0,0 +1,7 @@
+//1- determine the exact output of the following program [2 marks]
+#include <stdio.h>
+int main(void){
+   printf(" his\b\bat is a test \rT\nFor \"U\" and \that is mine");
+   return 0;
+}
+
